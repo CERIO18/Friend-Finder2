107ef0a5e41586559771afc82e8dd2341b4f3546
CERIO18 <ceurioste@derp.org>
2019-05-20 15:33:55 -0600

    fixing pictures

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [{
+    "name":"Happy",
+    "photo":"https://.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  
+  },{
+  "name":"Sad",
+    "photo":"http://.jpg",
+    "scores":[3,5,4,3,1,4,5,3,4,5]
+  },{
+    "name":"Goofy",
+    "photo":"https://.jpg",
+    "scores":[1,3,4,5,1,6,2,5,1,1]
+  },{
+    "name":"Sassy",
+    "photo":"http://.jpg",
+    "scores":[1,3,4,3,7,2,5,1,3,5]
+  }
+  
+  ]
+  
+  module.exports = friends;
