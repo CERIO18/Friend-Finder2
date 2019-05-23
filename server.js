ed3f37442775e9a503ed020d400b8d49f72c0ca0
CERIO18 <ceurioste@derp.org>
2019-05-22 23:20:00 -0600

    please work

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-// var bodyParser = require('body-parser');
 var express = require('express');
 var path = require("path");
-var friends = require('./FriendFinder/app/data/friends');
+var friends = require('./app/data/friends.js');
 
 var app = express();
 
@@ -9,15 +8,14 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.static('app/public'));
 
-//Express app 
+//Express 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./app/routing/apiRoutes.js")(app);
 
-require("./FriendFinder/app/../routing/apiRoutes")(app);
-
-require("./FriendFinder/app/../routing/htmlRoutes")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
-  console.log("App listing to PORT:" + PORT);
+  console.log("App listening to PORT:" + PORT);
 });
